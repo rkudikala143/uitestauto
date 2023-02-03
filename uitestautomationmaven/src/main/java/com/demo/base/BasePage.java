d35c51398c2de3dcbe64f29c0754cca971f07888
Srinivas Rapelli <srinivasrapelli1387@gmail.com>
2023-02-03 20:10:46 +0530

    Books_C108_C109_C111

--- uitestautomationmaven/src/main/java/com/demo/base/BasePage.java
+++ uitestautomationmaven/src/main/java/com/demo/base/BasePage.java
@@ -14,6 +14,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeSuite;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -79,6 +81,7 @@ public class BasePage {
 //		return driver.getTitle();
 //	}
 
+	@BeforeSuite
 	public static void setUp() {
 		// create an instance for Chrome Driver
 		driver = new ChromeDriver();
